ee4d7bb48f1494328fdca2938de8dd3fc0e34c39
A.J. Lauer Barinov <791798+andrewjl@users.noreply.github.com>
2025-08-29 09:39:47 -0700

    test: current simulation clock time

--- Tests/SimulationKitTests/ClockTests.swift
+++ Tests/SimulationKitTests/ClockTests.swift
@@ -9,16 +9,28 @@ import XCTest
 final class ClockTests: XCTestCase {
     func testClock() throws {
         let clock = Clock()
+        XCTAssertEqual(clock.current().time, Clock.startingTime)
 
         let tick = clock.next()
         XCTAssertEqual(tick.time, Clock.startingTime)
 
+        XCTAssertEqual(clock.current().time, Clock.startingTime + 1)
+
         let nextTick = clock.next()
         XCTAssertEqual(nextTick.time, 1)
+    }
+
+    func testClockReset() throws {
+        let clock = Clock()
+
+        let _ = clock.next()
 
         clock.reset()
 
         let resetTick = clock.next()
-        XCTAssertEqual(resetTick.time, Clock.startingTime)
+        XCTAssertEqual(
+            resetTick.time,
+            Clock.startingTime
+        )
     }
 }
